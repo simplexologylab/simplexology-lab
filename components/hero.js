9343618302fc90dd14de891e8a03d42b0c29ed7b
Ramsey Tisher <ramsey@simplexologylab.com>
2021-09-09 12:15:13 -0400

    added tailwind

---
+++ components/hero.js
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div className="min-h-screen flex flex-col place-content-center">
+      <div className="font-mono text-4xl md:text-6xl text-center">Simplexology Lab</div>
+      <div className="font-mono text-xl md:text-3xl text-center mt-3">
+        we make it simple
+      </div>
+      <div className="text-center mt-3">
+        <p className="font-mono text-xl md:text-2xl px-20">
+          Stop struggling with making the best technology for your business,
+          classroom, or even personal hobbies. Leverage our years of experience
+          implementing technology solutions to amplify your ideas into reality.
+        </p>
+      </div>
+    </div>
+  );
+}
